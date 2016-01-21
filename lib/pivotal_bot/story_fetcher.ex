4c5f550b9ba2f06f67f666224a5fdfee2b427e9b
Egor Lynko <flexoid@gmail.com>
2016-01-22 00:15:33 +0300

    Add module to fetch pivotal story title

---
+++ lib/pivotal_bot/story_fetcher.ex
@@ -0,0 +1,22 @@
+defmodule PivotalBot.StoryFetcher do
+  @token_header "X-TrackerToken"
+
+  @doc """
+  Fetches story title.
+  """
+  def fetch_title(story_id) do
+    story = fetch_story(story_id)
+    story["name"]
+  end
+
+  defp fetch_story(story_id) do
+    url = "https://www.pivotaltracker.com/services/v5/stories/#{story_id}"
+
+    HTTPoison.get!(url, [{@token_header, token}]).body
+    |> Poison.decode!
+  end
+
+  defp token do
+    Application.fetch_env!(:pivotal_bot, :pivotal_tracker_secret_token)
+  end
+end
